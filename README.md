c55692a515e7b432e2713497bfdf8ac02f5fab48
ERICH SAUER <erich.sauer@gmail.com>
2021-01-11 15:34:47 -0800

    adds html framework and some js

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# lab-01-quiz-page
+# THE PLAN
+
+## OUTCOMES
+- prompts user
+    - welcome to the test
+    - to continue or opt out
+    - to submit first name
+    - to submit last name
+    - to answer question 1
+    - to answer question 2
+    - to answer question 3
+- displays results of quiz
+
+HTML setup
+- set IDs for DOM elements
+    - button
+    - empty `<div>`
+- set up basic quiz content
+    - three `<p>`
+    - one image
+    - one UL
+    - on OL
+    - headers
+
+JS setup
+- grab DOM elements
+    - button
+    - empty `<div>`
+- add event listener
+    - button
+- respond to button click with
+    - alert "you're gonna take a quiz!"
+    - confirmation "do you wanna?"
+        - yes continues
+        - no exits cool zone with `return`
+    - prompt "what's your name?"
+    - prompt "what's your SSN?"
+    - question 1
+    - question 2
+    - question 3
+- tracks number of correct answers
+- displays score with name
--- app.js
+++ app.js
@@ -1,5 +1,37 @@
 // import functions and grab DOM elements
-
+const quizButton = document.getElementById('quiz-button');
+const quizResult = document.getElementById('quiz-results-div');
 // initialize state
-
 // set event listeners to update state and DOM
+quizButton.addEventListener('click', () => {
+    alert('So you wanna take a quiz about raccoons...');
+    
+    const confirmation = confirm('Are you ready to PROVE that you learned a lot about raccoons?');
+    
+    if(!confirmation) return;
+    
+    const firstName = prompt("What's your first name?")
+    const lastName = prompt("What's your last name?")
+    const ssn = Number(prompt("What's your Social Security Number?", '0'))
+    
+    if(ssn !== 0) alert("NEVER give your SSN to a pop-up prompt from the mid-90s! Try again.")
+    
+    let correctAnswers = 0
+
+    const answerOne = prompt("Do raccoons have a dog's stomach and a possum's heart?")
+
+    if (answerOne.charAt(0).toUpperCase() === 'Y') correctAnswers++;
+
+    const answerTwo = prompt("Is it scientifically speculated that raccoons are in fact an assemblage of very condensed squirrels?")
+
+
+    const answerThree = prompt('Are raccoons the child of a hamster and a witch?')
+
+
+
+
+    console.log(correctAnswers);
+})
+// - respond to button click with
+// - tracks number of correct answers
+// - displays score with name
